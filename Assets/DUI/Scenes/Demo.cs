0dd5293089340bfbcae853fdb8f74bc0db00a694
furaga <furaga.fukahori@gmail.com>
2020-08-15 23:52:04 +0900

    refactor

--- Assets/DUI/Scenes/Demo.cs
+++ Assets/DUI/Scenes/Demo.cs
@@ -10,37 +10,36 @@ public class Demo : MonoBehaviour
         onDUI();
     }
 
-    int pressCount_ = 0;
-
-    string textField = "DTextField";
-    float slider = 0.3f;
-    bool toggle = true;
+    int button_ = 0;
+    string textField_ = "DTextField";
+    float slider_ = 0.3f;
+    bool toggle_ = true;
 
     void onDUI()
     {
-        float offset = 0;
-        DUI.Box(new Rect(30 + offset, 30, 200, 30), "DBox");
-        if (DUI.Button(new Rect(30 + offset, 70, 200, 30), string.Format("DButton (Pressed {0} Times)", pressCount_)))
+        float ox = 0;
+        DUI.Box(new Rect(30 + ox, 30, 200, 30), "DBox");
+        if (DUI.Button(new Rect(30 + ox, 70, 200, 30), string.Format("DButton (Pressed {0} Times)", button_)))
         {
-            pressCount_ += 1;
+            button_ += 1;
         }
-        textField = DUI.TextField(new Rect(30 + offset, 110, 200, 30), textField);
-        DUI.Label(new Rect(30 + offset, 150, 200, 30), "DLabel");
-        slider = DUI.HorizontalSlider(new Rect(30 + offset, 190, 200, 30), slider, 0, 1);
-        toggle = DUI.Toggle(new Rect(30 + offset, 230, 200, 30), toggle, "DToggle");
+        textField_ = DUI.TextField(new Rect(30 + ox, 110, 200, 30), textField_);
+        DUI.Label(new Rect(30 + ox, 150, 200, 30), "DLabel");
+        slider_ = DUI.HorizontalSlider(new Rect(30 + ox, 190, 200, 30), slider_, 0, 1);
+        toggle_ = DUI.Toggle(new Rect(30 + ox, 230, 200, 30), toggle_, "DToggle");
     }
 
     void OnGUI()
     {
-        float offset = 240;
-        GUI.Box(new Rect(30 + offset, 30, 200, 30), "DBox");
-        if (GUI.Button(new Rect(30 + offset, 70, 200, 30), string.Format("DButton (Pressed {0} Times)", pressCount_)))
+        float ox = 240;
+        GUI.Box(new Rect(30 + ox, 30, 200, 30), "DBox");
+        if (GUI.Button(new Rect(30 + ox, 70, 200, 30), string.Format("DButton (Pressed {0} Times)", button_)))
         {
-            pressCount_ += 1;
+            button_ += 1;
         }
-        textField = GUI.TextField(new Rect(30 + offset, 110, 200, 30), textField);
-        GUI.Label(new Rect(30 + offset, 150, 200, 30), "DLabel");
-        slider = GUI.HorizontalSlider(new Rect(30 + offset, 190, 200, 30), slider, 0, 1);
-        toggle = GUI.Toggle(new Rect(30 + offset, 230, 200, 30), toggle, "DToggle");
+        textField_ = GUI.TextField(new Rect(30 + ox, 110, 200, 30), textField_);
+        GUI.Label(new Rect(30 + ox, 150, 200, 30), "DLabel");
+        slider_ = GUI.HorizontalSlider(new Rect(30 + ox, 190, 200, 30), slider_, 0, 1);
+        toggle_ = GUI.Toggle(new Rect(30 + ox, 230, 200, 30), toggle_, "DToggle");
     }
 }
