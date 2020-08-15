ff3bbfb3777c8d96888df07f608dbae9dbef915b
furaga <furaga.fukahori@gmail.com>
2020-08-15 19:58:24 +0900

    instanciate button when calling DUI.Button()

---
+++ Assets/DUI/Scenes/Demo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DUI.Button(new Rect(50, 50, 150, 30), "This is button");
+        
+    }
+}
---
+++ Assets/DUI/Scripts/DUI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DUI 
+{
+    enum UIType
+    {
+        Button,
+    }
+
+    class Element
+    {
+        public UIType uiType;
+        public GameObject gameObject;
+        public Rect position;
+        public Element(UIType uiType, GameObject gameObject, Rect position)
+        {
+            this.uiType = uiType;
+            this.gameObject = gameObject;
+            this.position = position;
+        }
+    }
+
+    static Dictionary<uint, Element> elementDict = new Dictionary<uint, Element>();
+    static Dictionary<UIType, GameObject> prefabDict = new Dictionary<UIType, GameObject>();
+    static uint counter_ = 0;
+
+    static void setup()
+    {
+    }
+
+    static GameObject search(UIType uiType, Rect position)
+    {
+        foreach (uint key in elementDict.Keys)
+        {
+            var elem = elementDict[key];
+            if (uiType == elem.uiType && position == elem.position)
+            {
+                return elem.gameObject;
+            }
+        }
+
+        if (false == prefabDict.ContainsKey(uiType))
+        {
+            prefabDict[uiType] = Resources.Load<GameObject>("Prefab/Button");
+        }
+
+        var canvas = GameObject.FindObjectOfType<Canvas>();
+        var gameObject = GameObject.Instantiate(prefabDict[uiType], canvas.gameObject.transform);
+        elementDict[counter_] = new Element(uiType, gameObject, position);
+        return gameObject;
+    }
+
+    public static bool Button(Rect position, string content)
+    {
+        setup();
+        var gameObject = search(UIType.Button, position);
+        var buttonText = gameObject.GetComponentInChildren<UnityEngine.UI.Text>();
+        buttonText.text = content;
+        return false;
+    }
+
+}
